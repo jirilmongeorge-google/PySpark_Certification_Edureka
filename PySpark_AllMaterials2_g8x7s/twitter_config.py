9e20eee5b16a32ab6ae3a24f1559526003f3d896
Jirilmon George <jirilmongeorge@gmail.com>
2020-08-01 20:04:55 +0530

    Update twitter_config.py

--- PySpark_AllMaterials2_g8x7s/twitter_config.py
+++ PySpark_AllMaterials2_g8x7s/twitter_config.py
@@ -1,4 +1,4 @@
-consumer_key = 'I7J2zKAcFN5DUrHXP7Spe6'
-consumer_secret = 'bC7SvPpdazrQWb1ipJtpkw7KerjmWMVYA5uk6vgUhaLel'
-access_token = '768020608487567360-DQ151N0wtbXYdNZf7a8'
-access_secret = 'WqVXE5C6CE9LwMSGyyK9exUmxVWsvKGd'
+consumer_key = 'consumer_key'
+consumer_secret = 'consumer_secret'
+access_token = 'access_token'
+access_secret = 'access_secret'
