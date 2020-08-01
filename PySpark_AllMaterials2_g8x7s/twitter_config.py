d36cf32b638a5fd35f49f3628416e914b1afd321
Jirilmon George <jirilmongeorge@gmail.com>
2020-08-01 19:56:50 +0530

    Add files via upload

---
+++ PySpark_AllMaterials2_g8x7s/twitter_config.py
@@ -0,0 +1,4 @@
+consumer_key = 'I7J2zKAcFN5DUrHXP7Spe6'
+consumer_secret = 'bC7SvPpdazrQWb1ipJtpkw7KerjmWMVYA5uk6vgUhaLel'
+access_token = '768020608487567360-DQ151N0wtbXYdNZf7a8'
+access_secret = 'WqVXE5C6CE9LwMSGyyK9exUmxVWsvKGd'
